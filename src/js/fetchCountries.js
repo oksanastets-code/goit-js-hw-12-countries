3ac779fc0c46b1395f1fce79ad68e9b855aa6ce8
Oksana Stets <oksanastets73@gmail.com>
2021-10-31 16:34:28 +0200

    updates fetchCountries.js

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,11 +1,14 @@
 // https://restcountries.com/
-// fetch('https://restcountries.com/v2/name/{name}');
-// console.log(r);
+
 import debounce from 'lodash.debounce';
 import { alert, notice, info, success, error } from '@pnotify/core';
 import '@pnotify/core/dist/BrightTheme.css';
 import countryCardTpl from '../templates/country-card.hbs';
+import countryListTpl from '../templates/country-list.hbs';
 
+fetch('https://restcountries.com/v2/name/italy').then(response => {
+    return response.json();
+  }).then(country => console.log(country));
 const refs = {
     cardContainer: document.querySelector('.js-card-container'),
     searchField: document.querySelector('input'),
@@ -19,23 +22,35 @@ function onSearch(event) {
     console.log(event.target.value);
     const searchKey = event.target.value;
     fetchCountry(searchKey)
-  .then(renderCountryCard)
-  .catch(onError);
+    .then(render)
+  .catch (error);
 }
 function fetchCountry(name) {
   return fetch(`https://restcountries.com/v2/name/${name}`).then(response => {
     return response.json();
   });
 }
-
+function render(value) {
+  console.log(value.length);
+  if (value.length === 1) { renderCountryCard(value); }
+  else if (value.length > 1 && value.length < 11) { renderCountryList(value); }
+  else if (!value.length) {
+    refs.cardContainer.innerHTML = '';
+    console.log(error(`${value.length} matches!`))
+  }
+  else {
+    console.log(error(`${value.length} matches! і це забагато`))
+  }
+}
+ 
 function renderCountryCard(country) {
   const markup = countryCardTpl(country);
   console.log(markup);
   refs.cardContainer.innerHTML = markup;
 }
-function onError(error) {
-    PNotify.error({
-  title: 'Oh No!',
-  text: 'Something terrible happened.'
-});
+function renderCountryList(value) {
+  const markupList = countryListTpl(value);
+  console.log(markupList);
+  refs.cardContainer.innerHTML = markupList;
 }
+   
